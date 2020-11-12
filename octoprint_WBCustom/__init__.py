108745f9e3a0da1638b42c9c3f8422b22646e961
paulilio <paulilio.ferreira@gmail.com>
2020-11-12 10:27:07 -0300

    testes

--- octoprint_WBCustom/__init__.py
+++ octoprint_WBCustom/__init__.py
@@ -4,12 +4,10 @@ from __future__ import absolute_import, unicode_literals
 import octoprint.plugin
 
 from .api import WBCustomApi
-from .api.astroprint.api import WBCustomAstroPrintApi
 
 class WBCustomPlugin(octoprint.plugin.StartupPlugin,
                        octoprint.plugin.TemplatePlugin,
-                       WBCustomApi,
-					   WBCustomAstroPrintApi):
+                       WBCustomApi):
     def on_after_startup(self):
         self._logger.info("WBCustomPlugin!")
 
