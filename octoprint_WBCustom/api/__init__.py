40a654c16bfd651ff3a2789625cf952992cae419
paulilio <paulilio.ferreira@gmail.com>
2020-11-26 09:52:53 -0300

    testes

--- octoprint_WBCustom/api/__init__.py
+++ octoprint_WBCustom/api/__init__.py
@@ -23,11 +23,11 @@ class WBCustomApi(octoprint.plugin.BlueprintPlugin):
     #@api.route("/settings/network/wifi-networks", methods=["GET"])
     @octoprint.plugin.BlueprintPlugin.route("/wifi", methods=["GET"])
     def getNetworkSettings(self):
-		nm = networkManager
+        nm = networkManager
 
-		return jsonify({
-			'networks': nm.getActiveConnections(),
-			'networkDeviceInfo': nm.networkDeviceInfo,
-			'hasWifi': nm.hasWifi(),
-			'storedWifiNetworks': nm.storedWifiNetworks()
-		})
+        return jsonify({
+            'networks': nm.getActiveConnections(),
+            'networkDeviceInfo': nm.networkDeviceInfo,
+            'hasWifi': nm.hasWifi(),
+            'storedWifiNetworks': nm.storedWifiNetworks()
+        })
