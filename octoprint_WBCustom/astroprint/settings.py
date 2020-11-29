78496b02640968f4ab8dade73c12d245397f7900
paulilio <paulilio.ferreira@gmail.com>
2020-11-29 12:37:07 -0300

    testes

--- octoprint_WBCustom/astroprint/settings.py
+++ octoprint_WBCustom/astroprint/settings.py
@@ -6,412 +6,118 @@ __copyright__ = "Copyright (C) 2016 3DaGoGo, Inc - Released under terms of the A
 ASSETS_DEBUG = True
 ASSETS_AUTO_BUILD = True
 
-APPNAME = 'AstroBox'
-instance = None
+#APPNAME = 'AstroBox'
+#instance = None
+_instance = None
 
-def settings(init=False, configfile=None, basedir=None):
-    global instance
-    if instance is None:
-        if init:
-            instance = Settings(configfile, basedir)
-        else:
-            raise ValueError('Settings not initialized yet')
-    return instance
+def settings():
+	global _instance
+
+	if _instance is None:
+
+		default_settings = {'serial': {'port': None,
+					'baudrate': None,
+					'autoconnect': True,
+					'log': False,
+					'dsrdtrFlowControl': False,
+					'rtsctsFlowControl': False,
+					'swFlowControl': True,
+					'timeout': {'detection': 0.5,
+								'connection': 2.0,
+								'communication': 10.0,
+								'first_contact': 20.0,
+								'temperature': 5,
+								'sdStatus': 1},
+					'additionalPorts': []},
+		'server': {'host': '0.0.0.0',
+					'port': 5000,
+					'firstRun': True,
+					'baseUrl': '',
+					'scheme': '',
+					'maxUploadSize': 200},
+		'camera': {'manager': 'gstreamer',
+					'encoding': 'h264',
+					'size': '640x480',
+					'framerate': '15/1',
+					'format': 'x-raw',
+					'pixelformat': 'YUYV',
+					'source': 'USB',
+					'debug-level': 0,
+					'graphic-debug': False,
+					'video-rotation': 0,
+					'inactivitySecs': 120.0,
+					'freq': 0},
+		'clearFiles': False,
+		'gcodeViewer': {'enabled': True,
+						'mobileSizeThreshold': 2097152,
+						'sizeThreshold': 20971520},
+		'feature': {'temperatureGraph': True,
+					'waitForStartOnConnect': False,
+					'alwaysSendChecksum': False,
+					'sdSupport': True,
+					'sdAlwaysAvailable': False,
+					'swallowOkAfterResend': True,
+					'repetierTargetTemp': False},
+		'folder': {'uploads': None,
+					'timelapse': None,
+					'timelapse_tmp': None,
+					'logs': None,
+					'virtualSd': None,
+					'userPlugins': None,
+					'tasks': None,
+					'manufacturerPkg': None},
+		'temperature': {'profiles': [{'name': 'ABS', 'extruder': 210, 'bed': 100}, {'name': 'PLA', 'extruder': 180, 'bed': 60}]}, 'printerParameters': {'movementSpeed': {'x': 6000,
+												'y': 6000,
+												'z': 700,
+												'e': 100},
+								'pauseTriggers': [], 'invertAxes': [], 'numExtruders': 1,
+								'extruderOffsets': [{'x': 0.0, 'y': 0.0}], 'bedDimensions': {'x': 200.0,
+												'y': 200.0, 'r': 100}},
+		'appearance': {'name': '',
+						'color': 'default'},
+		'controls': [], 'system': {'actions': []}, 'accessControl': {'enabled': True,
+							'userManager': 'astroprint.users.FilebasedUserManager',
+							'userfile': None,
+							'autologinLocal': False,
+							'localNetworks': [
+											'127.0.0.0/8'],
+							'autologinAs': None},
+		'cura': {'enabled': False,
+					'path': '/default/path/to/cura',
+					'config': '/default/path/to/your/cura/config.ini'},
+		'cloudSlicer': {'loggedUser': None},
+		'events': {'enabled': False,
+					'subscriptions': []},
+		'api': {'enabled': True,
+				'key': None,
+				'regenerate': True,
+				'allowCrossOrigin': True},
+		'terminalFilters': [{'name': 'Suppress M105 requests/responses', 'regex': '(Send: M105)|(Recv: ok T\\d*:)'}, {'name': 'Suppress M27 requests/responses', 'regex': '(Send: M27)|(Recv: SD printing byte)'}], 'devel': {'stylesheet': 'css',
+					'virtualPrinter': {'enabled': False,
+										'okAfterResend': False,
+										'forceChecksum': False,
+										'okWithLinenumber': False,
+										'numExtruders': 1,
+										'includeCurrentToolInTemps': True,
+										'hasBed': True,
+										'repetierStyleTargetTemperature': False,
+										'extendedSdFileList': False}},
+		'wifi': {'hotspotDevice': None,
+					'hotspotOnlyOffline': True},
+		'network': {'manager': 'debianNetworkManager',
+					'interface': 'wlan0',
+					'ssl': {'domain': None}},
+		'software': {'infoDir': None,
+						'variantFile': None,
+						'useUnreleased': False,
+						'lastCheck': None,
+						'channel': 20},
+		'printerSelected': None,
+		'materialSelected': None,
+		'qualitySelected': None,
+		'customQualitySelected': None}
 
 
-default_settings = {'serial': {'port': None, 
-              'baudrate': None, 
-              'autoconnect': True, 
-              'log': False, 
-              'dsrdtrFlowControl': False, 
-              'rtsctsFlowControl': False, 
-              'swFlowControl': True, 
-              'timeout': {'detection': 0.5, 
-                          'connection': 2.0, 
-                          'communication': 10.0, 
-                          'first_contact': 20.0, 
-                          'temperature': 5, 
-                          'sdStatus': 1}, 
-              'additionalPorts': []}, 
-   'server': {'host': '0.0.0.0', 
-              'port': 5000, 
-              'firstRun': True, 
-              'baseUrl': '', 
-              'scheme': '', 
-              'maxUploadSize': 200}, 
-   'camera': {'manager': 'gstreamer', 
-              'encoding': 'h264', 
-              'size': '640x480', 
-              'framerate': '15/1', 
-              'format': 'x-raw', 
-              'pixelformat': 'YUYV', 
-              'source': 'USB', 
-              'debug-level': 0, 
-              'graphic-debug': False, 
-              'video-rotation': 0, 
-              'inactivitySecs': 120.0, 
-              'freq': 0}, 
-   'clearFiles': False, 
-   'gcodeViewer': {'enabled': True, 
-                   'mobileSizeThreshold': 2097152, 
-                   'sizeThreshold': 20971520}, 
-   'feature': {'temperatureGraph': True, 
-               'waitForStartOnConnect': False, 
-               'alwaysSendChecksum': False, 
-               'sdSupport': True, 
-               'sdAlwaysAvailable': False, 
-               'swallowOkAfterResend': True, 
-               'repetierTargetTemp': False}, 
-   'folder': {'uploads': None, 
-              'timelapse': None, 
-              'timelapse_tmp': None, 
-              'logs': None, 
-              'virtualSd': None, 
-              'userPlugins': None, 
-              'tasks': None, 
-              'manufacturerPkg': None}, 
-   'temperature': {'profiles': [{'name': 'ABS', 'extruder': 210, 'bed': 100}, {'name': 'PLA', 'extruder': 180, 'bed': 60}]}, 'printerParameters': {'movementSpeed': {'x': 6000, 
-                                           'y': 6000, 
-                                           'z': 700, 
-                                           'e': 100}, 
-                         'pauseTriggers': [], 'invertAxes': [], 'numExtruders': 1, 
-                         'extruderOffsets': [{'x': 0.0, 'y': 0.0}], 'bedDimensions': {'x': 200.0, 
-                                           'y': 200.0, 'r': 100}}, 
-   'appearance': {'name': '', 
-                  'color': 'default'}, 
-   'controls': [], 'system': {'actions': []}, 'accessControl': {'enabled': True, 
-                     'userManager': 'astroprint.users.FilebasedUserManager', 
-                     'userfile': None, 
-                     'autologinLocal': False, 
-                     'localNetworks': [
-                                     '127.0.0.0/8'], 
-                     'autologinAs': None}, 
-   'cura': {'enabled': False, 
-            'path': '/default/path/to/cura', 
-            'config': '/default/path/to/your/cura/config.ini'}, 
-   'cloudSlicer': {'loggedUser': None}, 
-   'events': {'enabled': False, 
-              'subscriptions': []}, 
-   'api': {'enabled': True, 
-           'key': None, 
-           'regenerate': True, 
-           'allowCrossOrigin': True}, 
-   'terminalFilters': [{'name': 'Suppress M105 requests/responses', 'regex': '(Send: M105)|(Recv: ok T\\d*:)'}, {'name': 'Suppress M27 requests/responses', 'regex': '(Send: M27)|(Recv: SD printing byte)'}], 'devel': {'stylesheet': 'css', 
-             'virtualPrinter': {'enabled': False, 
-                                'okAfterResend': False, 
-                                'forceChecksum': False, 
-                                'okWithLinenumber': False, 
-                                'numExtruders': 1, 
-                                'includeCurrentToolInTemps': True, 
-                                'hasBed': True, 
-                                'repetierStyleTargetTemperature': False, 
-                                'extendedSdFileList': False}}, 
-   'wifi': {'hotspotDevice': None, 
-            'hotspotOnlyOffline': True}, 
-   'network': {'manager': 'debianNetworkManager', 
-               'interface': 'wlan0', 
-               'ssl': {'domain': None}}, 
-   'software': {'infoDir': None, 
-                'variantFile': None, 
-                'useUnreleased': False, 
-                'lastCheck': None, 
-                'channel': 20}, 
-   'printerSelected': None, 
-   'materialSelected': None, 
-   'qualitySelected': None, 
-   'customQualitySelected': None}
-valid_boolean_trues = [
- True, 'true', 'yes', 'y', '1']
+		_instance = defaults
 
-class Settings(object):
-
-    def __init__(self, configfile=None, basedir=None):
-        self._logger = logging.getLogger(__name__)
-        self.settings_dir = None
-        self._config = None
-        self._dirty = False
-        self._saveLock = threading.Lock()
-        if configfile is not None:
-            self._configfile = configfile
-        else:
-            if basedir:
-                settings_dir = basedir
-            else:
-                settings_dir = os.path.realpath(os.path.dirname(__file__) + '/../../local')
-            self._configfile = os.path.join(settings_dir, 'config.yaml')
-        self._init_settings_dir(basedir)
-        self._factoryConfigFile = os.path.join(os.path.dirname(self._configfile), 'config.factory')
-        self.load()
-        return
-
-    def _init_settings_dir(self, basedir):
-        if basedir is not None:
-            self.settings_dir = basedir
-        else:
-            self.settings_dir = self.getConfigFolder()
-        return
-
-    def _getDefaultFolder(self, type):
-        folder = default_settings['folder'][type]
-        if folder is None:
-            folder = os.path.join(self.settings_dir, type.replace('_', os.path.sep))
-        return folder
-
-    def load(self):
-        if os.path.exists(self._factoryConfigFile) and not os.path.exists(self._configfile):
-            shutil.copy(self._factoryConfigFile, self._configfile)
-        if os.path.exists(self._configfile) and os.path.isfile(self._configfile):
-            with open(self._configfile, 'r') as (f):
-                self._config = yaml.safe_load(f)
-        if not self._config:
-            self._config = {}
-
-    def save(self, force=False):
-        if not self._dirty and not force:
-            return
-        with self._saveLock:
-            with open(self._configfile, 'wb') as (configFile):
-                yaml.safe_dump(self._config, configFile, default_flow_style=False, indent='    ', allow_unicode=True)
-                self._dirty = False
-            self.load()
-
-    def get(self, path, asdict=False):
-        if len(path) == 0:
-            return
-        else:
-            config = self._config
-            defaults = default_settings
-            while len(path) > 1:
-                key = path.pop(0)
-                if key in config.keys() and key in defaults.keys():
-                    config = config[key]
-                    defaults = defaults[key]
-                elif key in defaults.keys():
-                    config = {}
-                    defaults = defaults[key]
-                else:
-                    return
-
-            k = path.pop(0)
-            if not isinstance(k, (list, tuple)):
-                keys = [
-                 k]
-            else:
-                keys = k
-            if asdict:
-                results = {}
-            else:
-                results = []
-            for key in keys:
-                if key in config.keys():
-                    value = config[key]
-                elif key in defaults:
-                    value = defaults[key]
-                else:
-                    value = None
-                if asdict:
-                    results[key] = value
-                else:
-                    results.append(value)
-
-            if not isinstance(k, (list, tuple)):
-                if asdict:
-                    return results.values().pop()
-                else:
-                    return results.pop()
-
-            else:
-                return results
-            return
-
-    def getInt(self, path):
-        value = self.get(path)
-        if value is None:
-            return
-        else:
-            try:
-                return int(value)
-            except ValueError:
-                self._logger.warn('Could not convert %r to a valid integer when getting option %r' % (value, path))
-                return
-
-            return
-
-    def getString(self, path):
-        value = self.get(path)
-        if value is None:
-            return
-        else:
-            try:
-                return str(value)
-            except ValueError:
-                self._logger.warn('Could not convert %r to a valid string when getting option %r' % (value, path))
-                return
-
-            return
-
-    def getFloat(self, path):
-        value = self.get(path)
-        if value is None:
-            return
-        else:
-            try:
-                return float(value)
-            except ValueError:
-                self._logger.warn('Could not convert %r to a valid integer when getting option %r' % (value, path))
-                return
-
-            return
-
-    def getBoolean(self, path):
-        value = self.get(path)
-        if value is None:
-            return
-        else:
-            if isinstance(value, bool):
-                return value
-            return value.lower() in valid_boolean_trues
-
-    def getBaseFolder(self, type):
-        if type not in default_settings['folder'].keys():
-            return
-        else:
-            folder = self.get(['folder', type])
-            if folder is None:
-                folder = self._getDefaultFolder(type)
-            if not os.path.isdir(folder):
-                os.makedirs(folder)
-            return folder
-
-    def getConfigFolder(self):
-        return os.path.dirname(self._configfile)
-
-    def getFeedbackControls(self):
-        feedbackControls = []
-        for control in self.get(['controls']):
-            feedbackControls.extend(self._getFeedbackControls(control))
-
-        return feedbackControls
-
-    def _getFeedbackControls(self, control=None):
-        if control['type'] == 'feedback_command' or control['type'] == 'feedback':
-            pattern = control['regex']
-            try:
-                matcher = re.compile(pattern)
-                return [(control['name'], matcher, control['template'])]
-            except:
-                pass
-
-        else:
-            if control['type'] == 'section':
-                result = []
-                for c in control['children']:
-                    result.extend(self._getFeedbackControls(c))
-
-                return result
-            return []
-
-    def getPauseTriggers(self):
-        triggers = {'enable': [], 'disable': [], 'toggle': []}
-        for trigger in self.get(['printerParameters', 'pauseTriggers']):
-            try:
-                regex = trigger['regex']
-                type = trigger['type']
-                if type in triggers.keys():
-                    re.compile(regex)
-                    triggers[type].append(regex)
-            except:
-                pass
-
-        result = {}
-        for type in triggers.keys():
-            if len(triggers[type]) > 0:
-                result[type] = re.compile(('|').join(map(lambda x: '(%s)' % x, triggers[type])))
-
-        return result
-
-    def set(self, path, value, force=False):
-        if len(path) == 0:
-            return
-        else:
-            config = self._config
-            defaults = default_settings
-            while len(path) > 1:
-                key = path.pop(0)
-                if key in config.keys() and key in defaults.keys():
-                    config = config[key]
-                    defaults = defaults[key]
-                elif key in defaults.keys():
-                    config[key] = {}
-                    config = config[key]
-                    defaults = defaults[key]
-                else:
-                    return
-
-            key = path.pop(0)
-            if not force and key in defaults.keys() and key in config.keys() and defaults[key] == value:
-                del config[key]
-                self._dirty = True
-            elif force or key not in config.keys() and defaults[key] != value or key in config.keys() and config[key] != value:
-                if value is None:
-                    if key in config:
-                        del config[key]
-                else:
-                    config[key] = value
-                self._dirty = True
-            return
-
-    def setInt(self, path, value, force=False):
-        if value is None:
-            self.set(path, None, force)
-            return
-        else:
-            try:
-                intValue = int(value)
-            except ValueError:
-                self._logger.warn('Could not convert %r to a valid integer when setting option %r' % (value, path))
-                return
-
-            self.set(path, intValue, force)
-            return
-
-    def setFloat(self, path, value, force=False):
-        if value is None:
-            self.set(path, None, force)
-            return
-        else:
-            try:
-                floatValue = float(value)
-            except ValueError:
-                self._logger.warn('Could not convert %r to a valid integer when setting option %r' % (value, path))
-                return
-
-            self.set(path, floatValue, force)
-            return
-
-    def setBoolean(self, path, value, force=False):
-        if value is None or isinstance(value, bool):
-            self.set(path, value, force)
-        elif value.lower() in valid_boolean_trues:
-            self.set(path, True, force)
-        else:
-            self.set(path, False, force)
-        return
-
-    def setBaseFolder(self, type, path, force=False):
-        if type not in default_settings['folder'].keys():
-            return
-        else:
-            currentPath = self.getBaseFolder(type)
-            defaultPath = self._getDefaultFolder(type)
-            if (path is None or path == defaultPath) and 'folder' in self._config.keys() and type in self._config['folder'].keys():
-                del self._config['folder'][type]
-                if not self._config['folder']:
-                    del self._config['folder']
-                self._dirty = True
-            elif path != currentPath and path != defaultPath or force:
-                if 'folder' not in self._config.keys():
-                    self._config['folder'] = {}
-                self._config['folder'][type] = path
-                self._dirty = True
-            return
+	return _instance
