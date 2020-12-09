b151b8cd8495d6fe3d7af90217462a8c800b0cae
paulilio <paulilio.ferreira@gmail.com>
2020-12-09 12:01:07 -0300

    testes

--- octoprint_WBCustom/astroprint/network/debian.py
+++ octoprint_WBCustom/astroprint/network/debian.py
@@ -335,7 +335,9 @@ class DebianNetworkManager(NetworkManagerBase):
 
             os.environ['SCAN_COUNT']  = str(1 + int((os.environ.get('SCAN_COUNT') if os.environ.get('SCAN_COUNT') is not None else 0)))
             #os.environ['SCAN_COUNT']  = str(1 + int(os.getenv['SCAN_COUNT']))
-            logger.info("ScanCount [%s]" % os.getenv('SCAN_COUNT'))
+            #logger.info("ScanCount [%s]" % os.getenv('SCAN_COUNT'))
+            if environ.get('SCAN_COUNT') > 1:
+                time.sleep(7)
 
             #if os.environ.get('SCAN_COUNT') is not None:
             #    self._countScan = os.getenv['SCAN_COUNT']
